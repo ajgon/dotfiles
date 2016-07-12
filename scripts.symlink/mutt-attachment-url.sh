778469350e7fd133182aa0f0ee553e6e415e1407
Igor Rzegocki <ajgon@irgon.com>
2016-07-12 19:04:15 +0200

    Fixed vimrc in linux, added whitespace support for mutt-attachment script

--- scripts.symlink/mutt-attachment-url.sh
+++ scripts.symlink/mutt-attachment-url.sh
@@ -2,11 +2,11 @@
 ATTACHMENTS_DIR="/var/www/mutt/"
 BASE_URL="https://ssl.irgon.com/mutt/"
 
-FILENAME="$1"
-BASENAME="$(basename ${FILENAME})"
+FILENAME="$@"
+BASENAME=$(basename "${FILENAME}")
 DESTINATION="${ATTACHMENTS_DIR}/${BASENAME}"
-mv $1 ${DESTINATION}
-chmod 644 ${DESTINATION}
-echo "https://ssl.irgon.com/mutt/${BASENAME}"
+mv "${FILENAME}" "${DESTINATION}"
+chmod 644 "${DESTINATION}"
+echo "https://ssl.irgon.com/mutt/$(echo ${BASENAME} | sed 's@ @%20@g')"
 read -n 1 -s
-rm -rf ${DESTINATION}
+rm -rf "${DESTINATION}"
